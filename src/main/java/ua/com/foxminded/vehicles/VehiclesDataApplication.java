c8230fea2c74b8221dc80bee020abc4ece7b0aab
Alexander <477163@gmail.com>
2023-07-18 18:36:54 +0300

    The VehicleControllerIntegrationTest class has been left others are completed

--- src/main/java/ua/com/foxminded/vehicles/VehiclesDataApplication.java
+++ src/main/java/ua/com/foxminded/vehicles/VehiclesDataApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VehiclesDataApplication {
-    
+
     public static final String DEV_PROFILE = "dev";
 
-	public static void main(String[] args) {
-	    SpringApplication application = new SpringApplication(VehiclesDataApplication.class);
-	    application.setAdditionalProfiles(DEV_PROFILE);
-	    application.run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(VehiclesDataApplication.class);
+        application.setAdditionalProfiles(DEV_PROFILE);
+        application.run(args);
+    }
 }
