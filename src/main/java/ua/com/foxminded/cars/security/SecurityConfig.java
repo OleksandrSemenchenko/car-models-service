ead5f12e4c028336c95c9abd82e7e11939b6eed7
Alex <477163@gamil.com>
2023-09-06 03:49:32 +0300

    Kyecloak authentication server with using jwks and PolicyEnforcer is marked by comment

--- src/main/java/ua/com/foxminded/cars/security/SecurityConfig.java
+++ src/main/java/ua/com/foxminded/cars/security/SecurityConfig.java
@@ -1,24 +1,53 @@
 package ua.com.foxminded.cars.security;
 
+import java.io.IOException;
+
+import org.keycloak.adapters.authorization.integration.jakarta.ServletPolicyEnforcerFilter;
+import org.keycloak.adapters.authorization.spi.ConfigurationResolver;
+import org.keycloak.adapters.authorization.spi.HttpRequest;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
+import org.keycloak.util.JsonSerialization;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    String jwkSetUri;
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
+        http
+        .authorizeHttpRequests(request -> request
                 .requestMatchers(HttpMethod.GET, "/v1/**").permitAll()
                 .anyRequest().authenticated())
             .cors(Customizer.withDefaults())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwkSetUri(jwkSetUri)));
+//            .addFilterAfter(createPolicyEnforcerFilter(), BearerTokenAuthenticationFilter.class);
         return http.build();
     }
+    
+//    private ServletPolicyEnforcerFilter createPolicyEnforcerFilter() {
+//        return new ServletPolicyEnforcerFilter(new ConfigurationResolver() {
+//            
+//            @Override
+//            public PolicyEnforcerConfig resolve(HttpRequest request) {
+//                try {
+//                    return JsonSerialization.readValue(getClass().getResourceAsStream("/policy-enforcer.json"), 
+//                                                       PolicyEnforcerConfig.class);
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        });
+//    }
 }
