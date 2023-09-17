368c68bfa8285edf43a2d48d090fdde311eeb10d
Alex <477163@gamil.com>
2023-09-17 16:13:57 +0300

    Reading the policy enforcer configuration file was replaced by using @ConfiguraionProperties

--- src/main/java/ua/com/foxminded/cars/security/SecurityConfig.java
+++ src/main/java/ua/com/foxminded/cars/security/SecurityConfig.java
@@ -1,16 +1,13 @@
 package ua.com.foxminded.cars.security;
 
-import java.io.IOException;
-
 import org.keycloak.adapters.authorization.integration.jakarta.ServletPolicyEnforcerFilter;
 import org.keycloak.adapters.authorization.spi.ConfigurationResolver;
 import org.keycloak.adapters.authorization.spi.HttpRequest;
 import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
-import org.keycloak.util.JsonSerialization;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -38,15 +35,10 @@ public class SecurityConfig {
     
     private ServletPolicyEnforcerFilter createPolicyEnforcerFilter() {
         return new ServletPolicyEnforcerFilter(new ConfigurationResolver() {
-            
+
             @Override
             public PolicyEnforcerConfig resolve(HttpRequest request) {
-                try {
-                    return JsonSerialization.readValue(getClass().getResourceAsStream(KEYCLOAK_CONFIG_FILE_PATH), 
-                                                       PolicyEnforcerConfig.class);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                return policyEnforcerConfig();
             }
         });
     }
@@ -54,5 +46,11 @@ public class SecurityConfig {
     @Bean
     JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
+    
+    @Bean
+    @ConfigurationProperties("keycloak.policy-enforcer-config")
+    PolicyEnforcerConfig policyEnforcerConfig() {
+        return new PolicyEnforcerConfig();
     }
 }
