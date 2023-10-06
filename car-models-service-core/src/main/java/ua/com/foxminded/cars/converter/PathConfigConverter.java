f0e4ecbef98f4207f75e2007b2d26b833b5b03b0
Kyber <477163@gmail.com>
2023-10-06 13:21:16 +0300

    component-tests: HttpMethod             : Response from server: 400 / Bad Request / Body : {"error":"unauthorized_client","error_description":"Invalid identity"} PolicyEnforcer       : Authorization failed

---
+++ car-models-service-core/src/main/java/ua/com/foxminded/cars/converter/PathConfigConverter.java
@@ -0,0 +1,18 @@
+package ua.com.foxminded.cars.converter;
+
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig.PathConfig;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class PathConfigConverter implements Converter<String, PathConfig> {
+
+    @Override
+    public PathConfig convert(String source) {
+        PathConfig path = new PathConfig();
+        path.setPath(source);
+        return path;
+    }
+}
