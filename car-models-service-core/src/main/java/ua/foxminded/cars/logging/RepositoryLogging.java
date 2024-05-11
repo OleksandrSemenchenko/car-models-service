31eaaab677a7ab50e1c1189b76c4f5d88e105808
Oleksandr Semenchenko <477163@gmail.com>
2024-05-11 22:42:32 +0300

    Add AOP loggin to the repository layer

--- car-models-service-core/src/main/java/ua/foxminded/cars/logging/RepositoryLogging.java
+++ car-models-service-core/src/main/java/ua/foxminded/cars/logging/RepositoryLogging.java
@@ -15,14 +15,32 @@
  */
 package ua.foxminded.cars.logging;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Aspect
 @Component
+@Slf4j
 public class RepositoryLogging {
 
-  @Pointcut("execution public . ua.foxminded.repository.* ")
+  @Pointcut("execution(public * ua.foxminded.cars.repository.*Repository.*(..))")
   void repositoryMethod() {}
+
+  @Around("repositoryMethod() ")
+  public Object addLoggingToRepositoryMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+    String className = joinPoint.getSignature().getDeclaringTypeName();
+    String methodName = joinPoint.getSignature().getName();
+    String arguments = Arrays.toString(joinPoint.getArgs());
+    log.warn(String.format("Request: %s.%s.s.(%s)", className, methodName, arguments));
+    Object result = joinPoint.proceed();
+    log.warn(String.format("Response: %s.%s.(%s) Returned: %s", className, methodName, arguments, result));
+    return result;
+  }
 }
