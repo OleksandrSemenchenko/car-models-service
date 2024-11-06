c4282713ea44206b7a7eb7e8b2a00b9d210e3ced
Oleksandr Semenchenko <477163@gmail.com>
2024-11-06 19:51:47 +0100

    [B-1] Change the application runner config

--- telegram-bot/src/main/java/ua/nicegrear/cars/bot/config/ApplicationRunnerConfig.java
+++ telegram-bot/src/main/java/ua/nicegrear/cars/bot/config/ApplicationRunnerConfig.java
@@ -1,22 +1,26 @@
 package ua.nicegrear.cars.bot.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ua.nicegrear.cars.bot.controller.BotController;
 
 @Configuration
-public class BotConfig {
+public class ApplicationRunnerConfig {
 
   @Value("${bot.token}")
   private String token;
 
   @Bean
-  TelegramBotsLongPollingApplication telegramBotApi() throws TelegramApiException {
-    TelegramBotsLongPollingApplication pollingApplication = new TelegramBotsLongPollingApplication();
-    pollingApplication.registerBot(token, new BotController(token));
-    return pollingApplication;
+  ApplicationRunner applicationRunner() {
+    return args -> {
+      try (TelegramBotsLongPollingApplication botsApplication =
+             new TelegramBotsLongPollingApplication();) {
+        botsApplication.registerBot(token, new BotController(token));
+        Thread.currentThread().join();
+      }
+    };
   }
 }
